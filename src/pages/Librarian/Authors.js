c0924f66ea02fc9fb60026ee879c3b749085a50c
Test <test@Arbisofts-MacBook-Pro.local>
2023-10-25 16:31:41 +0500

    feat: add, update, delete author

---
+++ src/pages/Librarian/Authors.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import axiosInstance from "../../axios";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import { Author_URL } from "../../utils";
+import Author from "../../Components/Librarian/Author";
+import AuthorForm from "../../Components/Librarian/AuthorForm";
+
+const defaultTheme = createTheme();
+
+const Authors = () => {
+    const [author, setAuthor] = useState([]);
+    const [isAddingAuthor, setIsAddingAuthor] = useState(false);
+
+    const getAuthor = async () => {
+        try {
+            //   const response = await axiosInstance.get(Author_URL);
+            let data = [
+                {
+                    "id": 1,
+                    "name": "Rylee Fletcher",
+                    "gender": "F",
+                    "email": "toselyr@mailinator.com"
+                },
+                {
+                    "id": 2,
+                    "name": "Ayesha",
+                    "gender": "F",
+                    "email": "ayeshasiddique1306@gmail.com"
+                }
+            ]
+            setAuthor(data);
+        } catch (error) {
+            console.log("Error loading data");
+        }
+    };
+
+    useEffect(() => {
+        getAuthor();
+    }, []);
+
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Container sx={{ py: 8 }} maxWidth="lg">
+                <Grid container justifyContent="flex-end" marginBottom="2%">
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        size="small"
+                        onClick={() => setIsAddingAuthor(true)}
+                    >
+                        Add new Author
+                    </Button>
+                </Grid>
+                <Grid container spacing={4}>
+                    {author.map((card) => (
+                        <Author request={card} updateAuthor={getAuthor} />
+                    ))}
+                </Grid>
+            </Container>
+            <AuthorForm open={isAddingAuthor} handleClose={() => setIsAddingAuthor(false)} updateAuthor={getAuthor} />
+        </ThemeProvider>
+    );
+};
+
+export default Authors;
