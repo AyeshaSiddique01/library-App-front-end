ab2670b81dd2c1331b30c7a5e26ae24fc7c1d8c4
Ayesha Siddique <ayesha.siddique@A006-00914.local>
2023-10-26 18:41:38 +0500

    feat: requested books page of librarian

--- src/pages/Librarian/BookRequested.js
+++ src/pages/Librarian/BookRequested.js
@@ -16,29 +16,9 @@ const RequestedBooks = () => {
 
   const getUserBookRequests = async () => {
     try {
-    //   const response = await axiosInstance.get(BOOK_REQUEST_URL);
-    //   setBookRequests(response.data);
-    let data =[
-        {
-            "id": 2,
-            "status": "accepted",
-            "requested_date": "2023-10-02T12:48:41.522706Z",
-            "issued_date": null,
-            "returned_date": null,
-            "book": 1,
-            "user": 5
-        },
-        {
-            "id": 2,
-            "status": "pending",
-            "requested_date": "2023-10-02T12:48:41.522706Z",
-            "issued_date": null,
-            "returned_date": null,
-            "book": 1,
-            "user": 5
-        }
-    ]
-    setBookRequests(data)
+      const response = await axiosInstance.get(BOOK_REQUEST_URL);
+      setBookRequests(response.data);
+      setBookRequests(data);
     } catch (error) {
       console.log("Error loading data");
     }
@@ -55,7 +35,10 @@ const RequestedBooks = () => {
         <Container sx={{ py: 8 }} maxWidth="md">
           <Grid container spacing={4}>
             {bookRequests.map((card) => (
-              <BookRequest request={card} updateRequests={getUserBookRequests} />
+              <BookRequest
+                request={card}
+                updateRequests={getUserBookRequests}
+              />
             ))}
           </Grid>
         </Container>
