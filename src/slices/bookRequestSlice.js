8f7b582f066519e9be7caa54d59967925cffe476
ayesha.siddique <ayesha.siddique@arbisoft.com>
2023-11-05 20:06:25 +0500

    Requested book redux updates

--- src/slices/bookRequestSlice.js
+++ src/slices/bookRequestSlice.js
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 import axiosInstance from "../axios";
 import { BOOK_REQUEST_URL } from "../utils/Constants";
-
 
 const fetchBookRequests = async () => {
   try {
@@ -13,6 +13,12 @@ const fetchBookRequests = async () => {
   }
 };
 
+export const addBookRequest = createAsyncThunk("addBookRequest", async (id) => {
+  return axiosInstance
+    .post(BOOK_REQUEST_URL, id)
+    .then((response) => response.data);
+});
+
 const initialState = {
   bookRequests: await fetchBookRequests(),
 };
@@ -21,14 +27,6 @@ const bookRequestsSlice = createSlice({
   name: "bookRequests",
   initialState,
   reducers: {
-    addBookRequest: (state, action) => {
-      state.bookRequests.push(action.payload);
-      try {
-        axiosInstance.post(BOOK_REQUEST_URL, action.payload);
-      } catch (error) {
-        console.log("error: ", error);
-      }
-    },
     deleteBookRequest: (state, action) => {
       state.bookRequests = state.bookRequests.filter(
         (bookRequest) => bookRequest.id !== action.payload
@@ -40,8 +38,20 @@ const bookRequestsSlice = createSlice({
       }
     },
     updateBookRequest: (state, action) => {
+      const { id, status } = action.payload;
+      console.log(status);
+      let updatedRequest = action.payload;
+      var today = new Date().toLocaleDateString("en-GB").split("/");
+      var formatted_today = today[2] + "-" + today[1] + "-" + today[0];
+
+      status === "issued" && (updatedRequest["issued_date"] = formatted_today);
+      status === "returned" &&
+        (updatedRequest["returned_date"] = formatted_today);
+
       state.bookRequests = state.bookRequests.map((bookRequest) =>
-        bookRequest.id === action.payload.id ? action.payload.status : bookRequest["status"]
+        bookRequest.id === id
+          ? { ...bookRequest, ...updatedRequest }
+          : bookRequest
       );
       try {
         axiosInstance.patch(
@@ -53,7 +63,13 @@ const bookRequestsSlice = createSlice({
       }
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(addBookRequest.fulfilled, (state, action) => {
+      state.bookRequests.push(action.payload);
+    });
+  },
 });
 
-export const { addBookRequest, deleteBookRequest, updateBookRequest } = bookRequestsSlice.actions;
+export const { deleteBookRequest, updateBookRequest } =
+  bookRequestsSlice.actions;
 export default bookRequestsSlice.reducer;
